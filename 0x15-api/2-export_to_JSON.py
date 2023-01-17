d9595767140f4f0c9129a20a762a461b910d4210
Cayvoh-K <muhiakevin12@gmail.com>
2023-01-17 03:56:49 -0800

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId":
-        user_id}).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
