d647efce9d91d9baaa61294fad705f5be47e0a3e
Cayvoh-K <muhiakevin12@gmail.com>
2023-01-20 08:11:52 +0000

    gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,23 +7,23 @@ Implemented using recursion
 import json
 import urllib.request
 
+
 def get_employee_todo_list(employee_id):
     url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
     response = urllib.request.urlopen(url)
     todos = json.loads(response.read())
-    
     employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     employee_response = urllib.request.urlopen(employee_url)
     employee = json.loads(employee_response.read())
     name = employee['name']
-    
     completed_todos = [todo for todo in todos if todo['completed']]
     num_completed = len(completed_todos)
     num_total = len(todos)
-    
+
     print(f'Employee {name} is done with tasks({num_completed}/{num_total}):')
     for todo in completed_todos:
         print(f'\t{todo["title"]}')
+
 
 employee_id = input("Enter employee id: ")
 get_employee_todo_list(int(employee_id))
