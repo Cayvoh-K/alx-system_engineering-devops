338aa57eab1b11cf396f979f919f59a9bec7080d
Cayvoh-K <muhiakevin12@gmail.com>
2023-01-31 16:57:58 +0000

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
-function that queries the reddit API and returns the number of subscribers
-"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    headers = {'User-Agent': 'MyBot/1.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, headers=headers)
-    
+    """
+    Queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+        return response.json().get('data').get('subscribers')
     else:
         return 0
